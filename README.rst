b6f1a540a76afa2fd88d13cf1f00136d413f82a7
Kouhei Maeda <mkouhei@palmtb.net>
2016-04-22 13:46:34 +0900

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,40 @@
+========
+ pguard
+========
+
+TODO: Rewrite me. Append your package's long description.
+
+
+
+Requirements
+============
+
+* Python 2.7 or Python 3.3 over or PyPy 2.4.0 over
+
+Features
+========
+
+* ToDo: Rewrite me.
+
+Setup
+=====
+
+::
+
+  $ pip install --user pguard
+  or
+  (venv)$ pip install pguard
+
+Usage
+=====
+
+ToDo: Rewrite me.
+
+::
+
+  $ python
+  >>> import pguard
+  >>> pguard.sample.hello()
+  'hello'
+  >>>
+
---
+++ setup.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""setup.py"""
+
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', 'Arguments to pass to tox')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        import shlex
+        if self.tox_args:
+            errno = tox.cmdline(args=shlex.split(self.tox_args))
+        else:
+            errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+def read_content(filepath):
+    with open(filepath) as fobj:
+        return fobj.read()
+
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: "
+    "GNU Lesser General Public License v3 or later (LGPLv3+)",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+]
+
+
+long_description = (
+    read_content("README.rst") +
+    read_content(os.path.join("docs/source", "CHANGELOG.rst")))
+
+requires = ['setuptools']
+
+extras_require = {
+    'reST': ['Sphinx'],
+    }
+if os.environ.get('READTHEDOCS', None):
+    extras_require['reST'].append('recommonmark')
+
+with open('requirements.txt', 'w') as _file:
+    _file.write('\n'.join(requires))
+
+with open('extras_requirement.txt', 'w') as _file:
+    _file.write('\n'.join(extras_require.get('reST')))
+
+setup(name='pguard',
+      version='0.1.0',
+      description='Guard like Haskell for Python.',
+      long_description=long_description,
+      author='Kouhei Maeda',
+      author_email='mkouhei@palmtb.net',
+      url='mkouhei',
+      classifiers=classifiers,
+      packages=['pguard'],
+      data_files=[],
+      install_requires=requires,
+      include_package_data=True,
+      extras_require=extras_require,
+      tests_require=['tox'],
+      cmdclass={'test': Tox},)
